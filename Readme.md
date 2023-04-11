7ad31567bf442b18b65a0afdcd2aa8eaf2f58ecf
Scott Shepherd <scottshep86@gmail.com>
2023-04-11 14:56:51 +0200

    first commit from the game

---
+++ Readme.md
@@ -0,0 +1,40 @@
+<h1>Red Beards Lost Treasure</h1>
+
+<h2>Description</h2>
+
+<p>Red Beards Lost Treasure is a game where the player has to avoid skeletons and crabs horizontally whilst collecting coins to fill up your tresure chest. All obstacles appear from the left of the screen and move to your character on the right side of the screen. You have beat the game when you have collected enough coins to fill up the treasure chest or you lose the game if the skeletons or crabs manage to get you.</p>
+
+<h2>MVP (DOM - CANVAS)</h2>
+<ul>
+<li>game has a character (pirate) that moves vertically</li>
+<li>ship shoots cannonballs up</li>
+<li>skeletons, crabs and coins appear randomly from the left of the screen</li>
+<li>being caught by a skeleton or crab will end the game</li>
+<li>collecting enough coins to fill the chest will end the game</li>
+</ul>
+
+<h2>Backlog</h2>
+<ul>
+<li>switch between characters</li>
+<li>add a tresure chest</li>
+<li>add music to the game</li>
+</ul>
+
+
+
+<h2>States y States Transitions</h2>
+<h4>Definition of the different states and their transition (transition functions)</h4>
+<ul>
+<li>landingScreen</li>
+<li>gameScreen</li>
+<li>gameOverScreen</li>
+<li>gameWonScreen</li>
+</ul>
+
+<h2>Links</h2>
+<ul>
+<li><a href="https://trello.com/b/cDM8GUmZ/red-beards-lost-treasure">Trello Link</a></li>
+<li><a href="">Slides Link</a></li>
+<li><a href="https://github.com/ScottShep86/red-beards-lost-treasure-game">Github repository Link</a></li>
+<li><a href="">Deployment Link</a></li>
+</ul>
---
+++ index.js
@@ -0,0 +1,400 @@
+const canvas = document.getElementById("canvas");
+//canvas.style.border = "2px solid black";
+const ctx = canvas.getContext("2d");
+
+let startScreen = document.querySelector(".game-intro");
+let creditLogo = document.querySelector(".credit-logo");
+let restartBtn = document.querySelector("#restart-btn");
+
+let gameBackground = new Image();
+gameBackground.src = "images/parchmentAncient.png";
+let gameBackground1X = 0;
+let gameBackground2X = -canvas.width;
+
+let larsPirate = new Image();
+larsPirate.src = "images/lars.png";
+
+let skeleton = new Image();
+skeleton.src = "images/skeleton.png";
+
+let crab = new Image();
+crab.src = "images/redCrab.png";
+
+let gameOverSkull = new Image();
+gameOverSkull.src = "images/pirate skull.png";
+let gameOverSkullWidth = 400;
+let gameOverSkullHeight = 400;
+
+let tree = new Image();
+tree.src = "images/treePineTallLarge.png";
+
+let coin = new Image();
+coin.src = "images/coin.png";
+
+let larsPirateWidth = 70;
+let larsPirateHeight = 90;
+let larsPirateX = canvas.width - larsPirateWidth - 20;
+
+let isMovingUp = false;
+let isMovingDown = false;
+let larsPirateY = canvas.height / 2 - larsPirateHeight / 2;
+
+let gameOver = false;
+let animateId;
+let score = 0;
+//let wonGame = score >= 17;
+
+let skeletons = [];
+let crabs = [];
+let trees = [];
+let coins = [];
+
+class Skeleton {
+  constructor(y) {
+    this.xPos = -50;
+    this.yPos = y;
+    this.width = 70;
+    this.height = 90;
+  }
+
+  move() {
+    this.xPos += 4;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    // xPos, yPos, width, height
+    ctx.drawImage(skeleton, this.xPos, this.yPos, this.width, this.height);
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  checkCollision() {
+    if (
+      larsPirateY < this.yPos + this.height &&
+      larsPirateY + larsPirateHeight > this.yPos &&
+      larsPirateX < this.xPos + this.width &&
+      larsPirateWidth - 60 + larsPirateX > this.xPos
+    ) {
+      // Collision detected!
+      // Game Over
+      gameOver = true;
+      console.log("Collision");
+    }
+  }
+}
+
+class Crab {
+  constructor(y) {
+    this.xPos = -50;
+    this.yPos = y;
+    this.width = 150;
+    this.height = 99;
+  }
+
+  move() {
+    this.xPos += 1;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    // xPos, yPos, width, height
+    ctx.drawImage(crab, this.xPos, this.yPos, this.width, this.height);
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  checkCollision() {
+    if (
+      larsPirateY < this.yPos + this.height &&
+      larsPirateY + (larsPirateHeight - 60) > this.yPos &&
+      larsPirateX < this.xPos + this.width &&
+      larsPirateWidth - 60 + larsPirateX > this.xPos
+    ) {
+      // Collision detected!
+      // Game Over
+      gameOver = true;
+      console.log("Collision");
+    }
+  }
+}
+
+class Tree {
+  constructor(y) {
+    this.xPos = -50;
+    this.yPos = y;
+    this.width = 64;
+    this.height = 128;
+  }
+
+  move() {
+    this.xPos += 3;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    // xPos, yPos, width, height
+    ctx.drawImage(tree, this.xPos, this.yPos, this.width, this.height);
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+class Coin {
+  constructor(y) {
+    this.xPos = -50;
+    this.yPos = y;
+    this.width = 50;
+    this.height = 50;
+  }
+
+  move() {
+    this.xPos += 3;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    // xPos, yPos, width, height
+    ctx.drawImage(coin, this.xPos, this.yPos, this.width, this.height);
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  checkCollision() {
+    if (
+      larsPirateY < this.yPos + this.height &&
+      larsPirateY + (larsPirateHeight - 60) > this.yPos &&
+      larsPirateX < this.xPos + this.width &&
+      larsPirateWidth - 60 + larsPirateX > this.xPos
+    ) {
+      // Collision detected!
+      // 1 coin in the treasure chest
+      score += 1;
+    }
+  }
+}
+
+const drawBackground = () => {
+  ctx.beginPath();
+  ctx.drawImage(
+    gameBackground,
+    gameBackground1X,
+    0,
+    canvas.width,
+    canvas.height
+  );
+  ctx.drawImage(
+    gameBackground,
+    gameBackground2X,
+    0,
+    canvas.width,
+    canvas.height
+  );
+  gameBackground1X += 4;
+  gameBackground2X += 4;
+  if (gameBackground1X > canvas.width) {
+    gameBackground1X = -canvas.width;
+  }
+  if (gameBackground2X > canvas.width) {
+    gameBackground2X = -canvas.width;
+  }
+  ctx.closePath();
+};
+
+const drawLarsPirate = () => {
+  ctx.beginPath();
+  ctx.drawImage(
+    larsPirate,
+    larsPirateX,
+    larsPirateY,
+    larsPirateWidth,
+    larsPirateHeight
+  );
+  ctx.closePath();
+};
+
+const drawScore = () => {
+    ctx.beginPath();
+    ctx.font = "20pt TreasureMapDeadhand-yLA3";
+    ctx.fillText(`Chest : ${score}`, 10, 30);
+    ctx.closePath();
+}
+
+const animate = () => {
+  drawBackground();
+  drawLarsPirate();
+  drawScore()
+
+  const skeletonsStillInScreen = [];
+
+  console.log(skeletons);
+  skeletons.forEach((skeleton) => {
+    skeleton.draw();
+    skeleton.checkCollision();
+    skeleton.move();
+    // Is my obstacle still in the screen
+    if (skeleton.xPos < canvas.width) {
+      skeletonsStillInScreen.push(skeleton);
+    }
+  });
+  skeletons = skeletonsStillInScreen;
+
+  if (animateId % 100 === 0) {
+    skeletons.push(new Skeleton(Math.random() * (canvas.height - 90)));
+  }
+
+  const crabsStillInScreen = [];
+
+  console.log(crabs);
+  crabs.forEach((crab) => {
+    crab.draw();
+    crab.checkCollision();
+    crab.move();
+    // Is my obstacle still in the screen
+    if (crab.xPos < canvas.width) {
+      crabsStillInScreen.push(crab);
+    }
+  });
+  crabs = crabsStillInScreen;
+
+  if (animateId % 600 === 0) {
+    crabs.push(new Crab(Math.random() * (canvas.height - 99)));
+  }
+
+  const treesStillInScreen = [];
+
+  console.log(trees);
+  trees.forEach((tree) => {
+    tree.draw();
+    tree.move();
+    // Is my obstacle still in the screen
+    if (tree.xPos < canvas.width) {
+      treesStillInScreen.push(tree);
+    }
+  });
+  trees = treesStillInScreen;
+
+  if (animateId % 400 === 0) {
+    trees.push(new Tree(Math.random() * (canvas.height - 200)));
+  }
+
+  const coinsStillInScreen = [];
+
+  console.log(coins);
+  coins.forEach((coin) => {
+    coin.draw();
+    coin.checkCollision();
+    coin.move();
+    // Is my obstacle still in the screen
+    if (coin.xPos < canvas.width) {
+      coinsStillInScreen.push(coin);
+    }
+  });
+  coins = coinsStillInScreen;
+
+  if (animateId % 600 === 0) {
+    coins.push(new Coin(Math.random() * (canvas.height - 99)));
+  }
+
+  if (isMovingUp) {
+    larsPirateY -= 2;
+  } else if (isMovingDown) {
+    larsPirateY += 2;
+  }
+
+  if (gameOver) {
+    cancelAnimationFrame(animateId);
+    ctx.font = "30pt TreasureMapDeadhand-yLA3";
+    ctx.fillText("GAME OVER", canvas.width / 2 - 125, canvas.height / 2 - 140);
+    ctx.drawImage(
+      gameOverSkull,
+      280,
+      100,
+      gameOverSkullWidth,
+      gameOverSkullHeight
+    );
+  } else {
+    animateId = requestAnimationFrame(animate);
+  }
+
+  /*if (wonGame) {
+    cancelAnimationFrame(animateId);
+    ctx.font = "30pt TreasureMapDeadhand-yLA3";
+    ctx.fillText("YOU WON!", canvas.width / 2 - 125, canvas.height / 2 - 140);
+    ctx.drawImage(
+      gameOverSkull,
+      280,
+      100,
+      gameOverSkullWidth,
+      gameOverSkullHeight
+    );
+  } else {
+    animateId = requestAnimationFrame(animate);
+  }*/
+
+};
+
+window.onload = () => {
+  canvas.style.display = "none";
+  restartBtn.style.display = "none";
+  document.getElementById("start-btn").onclick = () => {
+    startGame();
+  };
+
+  function startGame() {
+    //console.log("Let's GO!");
+    canvas.style.display = "block";
+    startScreen.style.display = "none";
+    creditLogo.style.display = "none";
+
+    animate();
+  }
+
+  document.addEventListener("keydown", (event) => {
+    console.log(event);
+    if (event.key === "ArrowUp") {
+      isMovingUp = true;
+    }
+    if (event.key === "ArrowDown") {
+      isMovingDown = true;
+    }
+    console.log({ isMovingUp, isMovingDown });
+  });
+
+  document.addEventListener("keyup", (event) => {
+    console.log(event);
+    if (event.key === "ArrowUp") {
+      isMovingUp = false;
+    }
+    if (event.key === "ArrowDown") {
+      isMovingDown = false;
+    }
+    console.log({ isMovingUp, isMovingDown });
+  });
+
+  document.addEventListener("keydown", (event) => {
+    console.log(event);
+    if (event.key === "r" || event.key === "R") {
+      larsPirate.src = "images/ruby.png";
+    }
+    if (event.key === "l" || event.key === "L") {
+      larsPirate.src = "images/lars.png";
+    }
+    if (event.key === "n" || event.key === "N") {
+      larsPirate.src = "images/nightwing.png";
+    }
+    if (event.key === "b" || event.key === "B") {
+      skeleton.src = "images/bane.png";
+    }
+  });
+  document.addEventListener("keyup", (event) => {
+    console.log(event);
+    if (event.key === "b" || event.key === "B") {
+      skeleton.src = "images/skeleton.png";
+    }
+  });
+};
